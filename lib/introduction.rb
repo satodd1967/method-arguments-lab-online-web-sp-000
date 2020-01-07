c86e9e9079c72f68b6b36b4b90693ca79f18b286
Seth Todd <seth.todd@outlook.com>
2020-01-07 03:16:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,7 @@
-def introduction(name, language)
+def introduction(name)
   puts "Hi, my name is #{name}!"
 end
 
-def introduction
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I'm learning to program in #{language}."
+end
